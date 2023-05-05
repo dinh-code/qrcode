956510a2bd85e3307c49c2fcb45237839c7c1c77
dinh-code <63176375+dinh-code@users.noreply.github.com>
2023-05-05 16:00:28 +0700

    fix

--- qrPrint.js
+++ qrPrint.js
@@ -1,3 +1,13 @@
+/*document.getElementById('hide').addEventListener('change', function(){
+    if (this.checked){
+        document.getElementsByClassName('left').forEach(e => {
+            e.style.display = "none";
+        });
+    }
+})*/
+
+
+
 new QRCode(document.getElementById("qr1"), {text: "nhakhoasv.com/?okey=23051655", width: 200, height: 200});
 new QRCode(document.getElementById("qr2"), {text: "nhakhoasv.com/?okey=23051655", width: 200, height: 200});
 new QRCode(document.getElementById("qr3"), {text: "nhakhoasv.com/?okey=23051655", width: 200, height: 200});
