24bceb855e837138d344ebc23548b1227e0e5bbf
dinh-code <63176375+dinh-code@users.noreply.github.com>
2023-05-05 10:51:49 +0700

    first

---
+++ qrPrint.js
@@ -0,0 +1,4 @@
+new QRCode(document.getElementById("qr1"), {text: "nhakhoasv.com/?okey=23051655", width: 200, height: 200});
+new QRCode(document.getElementById("qr2"), {text: "nhakhoasv.com/?okey=23051655", width: 200, height: 200});
+new QRCode(document.getElementById("qr3"), {text: "nhakhoasv.com/?okey=23051655", width: 200, height: 200});
+new QRCode(document.getElementById("qr4"), {text: "nhakhoasv.com/?okey=23051655", width: 200, height: 200});
