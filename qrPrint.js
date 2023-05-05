1cccb92e3bdfcea20eb151f8ed2bcd75aefd144b
dinh-code <dinhphuocan@gmail.com>
2023-05-05 21:41:06 +0700

    update

--- qrPrint.js
+++ qrPrint.js
@@ -1,14 +1,24 @@
-/*document.getElementById('hide').addEventListener('change', function(){
-    if (this.checked){
-        document.getElementsByClassName('left').forEach(e => {
-            e.style.display = "none";
-        });
-    }
-})*/
 
+//new QRCode(document.getElementById("qr1"), {text: "nhakhoasv.com/?okey=23051655", width: 200, height: 200});
 
+var qrText = new URL(location.href).searchParams.get("qr");
+var qrHead = new URL(location.href).searchParams.get("h");
+var isHideInfo = new URL(location.href).searchParams.get("hide")*1;
+var qrArr = decodeURIComponent(qrText).split(",");
 
-new QRCode(document.getElementById("qr1"), {text: "nhakhoasv.com/?okey=23051655", width: 200, height: 200});
-new QRCode(document.getElementById("qr2"), {text: "nhakhoasv.com/?okey=23051655", width: 200, height: 200});
-new QRCode(document.getElementById("qr3"), {text: "nhakhoasv.com/?okey=23051655", width: 200, height: 200});
-new QRCode(document.getElementById("qr4"), {text: "nhakhoasv.com/?okey=23051655", width: 200, height: 200});
+for (let i = 0; i < qrArr.length; i+=4) {
+    document.body.innerHTML +=
+    `<div class="item">
+        <div class="head">NHA KHOA SV | ${qrHead}</div>
+        <div class="tbody">
+            <div class="left" id="qr${i}"></div>
+            <div class="right">${qrArr[i]}</div>
+        </div>
+        <div class="foot">Số lg: ${qrArr[i+1]} (${qrArr[i+2]}) - ${qrArr[i+3]}</div>
+    </div>`;
+    //new QRCode(document.getElementById("qr"+i), {text: "nhakhoasv.com/?okey="+qrHead, width: 200, height: 200});
+}
+
+for (let i = 0; i < qrArr.length; i+=4) {
+    new QRCode(document.getElementById("qr"+i), {text: "nhakhoasv.com/?okey="+qrHead, width: 200, height: 200});
+}
